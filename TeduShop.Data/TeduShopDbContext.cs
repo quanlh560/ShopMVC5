dd4a947f8bdb773f5c0f8e739bd0946cdb704d53
DELLLK\m4600nvidia <haiquank56cn@gmail.com>
2016-12-04 23:13:47 +0700

    Update project data

---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data
+
+{
+    public class TeduShopDbContext : DbContext
+    {
+        public TeduShopDbContext() : base("TeduShopConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        //public DbSet<Footer> Footers { set; get; }
+    }
+}
